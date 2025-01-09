e6756029562fadc4f5ecc684db8471c1067fdbb2
Wakar <waqar786@hotmail.de>
2025-01-10 00:34:37 +0100

    dlete

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,14 +3,7 @@ import { initializeAuth, getReactNativePersistence, getAuth } from 'firebase/aut
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyClW-rf6bD81ytSSU6DMgz3HZNcKncvnVE",
-  authDomain: "chanda-app-15b29.firebaseapp.com",
-  projectId: "chanda-app-15b29",
-  storageBucket: "chanda-app-15b29.firebasestorage.app",
-  messagingSenderId: "349759037686",
-  appId: "1:349759037686:android:5eb760a9c78d868b670cd7"
-};
+
 
 // Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
